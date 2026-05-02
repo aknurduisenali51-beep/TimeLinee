bd617c3acf32c9fd58953cca73ecde49c277f0cc
aknurduisenali51-beep <aknurduisenali51@gmail.com>
2026-05-03 03:07:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -200,4 +200,5 @@ def admin_delete_product(product_id):
 # ---------- Run ----------
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
